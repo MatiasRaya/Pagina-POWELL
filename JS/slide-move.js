1f875fe0840b97f5323145ed6eb41872e9c65a6c
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-06-20 09:52:33 -0300

    Se creo archivo que permite movimiento de las slides junto con el coloreado de las esferas indicadoras de posición

---
+++ JS/slide-move.js
@@ -0,0 +1,36 @@
+let index = 1;
+viewSlide(index);
+
+function nextSlider(n) {
+    viewSlide(index+=n);
+}
+
+function positionSlide(n) {
+    viewSlide(index=n);
+}
+
+setInterval(function time() {
+    viewSlide(index+=1)
+},2000);
+
+function viewSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName('body-slider');
+    let bars = document.getElementsByClassName('bar');
+
+    if(n > slides.length) {
+        index = 1;
+    }
+    if(n < 1) {
+        index = slides.length();
+    }
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < bars.length; i++) {
+        bars[i].className = bars[i].className.replace(" active", "");
+    }
+
+    slides[index-1].style.display = 'flex';
+    bars[index-1].className += ' active';
+}
